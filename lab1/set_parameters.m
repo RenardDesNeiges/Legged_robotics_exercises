cdb4ac80c2072c6bad11cb967621c73763242dde
Titouan Renard <titouan.renard@gmail.com>
2021-10-15 16:32:44 +0200

    init

---
+++ lab1/set_parameters.m
@@ -0,0 +1,16 @@
+%%
+% Set the parameters of the double pendulum. The parameters include: 
+%   + the masses m1, m2  
+%   + the link lenghts l1, l2
+%   + the gravity acceleration value g
+function [m1, m2, l1, l2, g] = set_parameters()
+
+    m1 = 7;
+    m2 = 7;
+    
+    l1 = 0.5;
+    l2 = 0.5;
+    
+    g = 9.81;
+
+end
