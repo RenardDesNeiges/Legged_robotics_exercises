6802065d6d89c6a4c68b55906a5abb2b9141f414
Titouan Renard <titouan.renard@gmail.com>
2021-10-18 22:26:02 +0200

    impact_error

--- lab2/dynamics/impact.m
+++ lab2/dynamics/impact.m
@@ -8,17 +8,13 @@ q_p(1, 1) = q_m(2, 1); % angular positions before/after impact
 q_p(2, 1) = q_m(1, 1);
 q_p(3, 1) = q_m(3, 1);
 
-dq_p(1, 1) = dq_m(2, 1); % angular positions before/after impact
-dq_p(2, 1) = dq_m(1, 1);
-dq_p(3, 1) = dq_m(3, 1);
-
 A_m = eval_A_m(q_m);
-A_p = eval_A_p(dq_m);
+A_p = eval_A_p(q_p);
 
 % A_p dq_p = A_m dq_m
 % Note: To solve the equation Ax = b you could use x = A \ b to avoid taking
 % inverse of A. 
 
-q_p = inv(A_p) * A_m * dq_m;
+dq_p = inv(A_p) * A_m * dq_m;
 
 end
